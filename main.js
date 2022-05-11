648bf2590d1df756e2c73b85bc21505299ccf176
LucasSPaiva <lukas.souza.paiva.9@gmail.com>
2022-05-11 16:26:26 -0300

    ⬆ Add section depositions, about and contact

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
-alert('Não está acabado')
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 1.3,
+	direction: 'horizontal',
+	loop: true,
+	mousewheel: true,
+	keyboard: true,
+	centeredSlides: true,
+	centeredSlidesBounds: true,
+	grabCursor: true,
 
+	pagination: {
+		el: '.swiper-pagination'
+	}
+})
+
+const swiperAbout = new Swiper('.swiperAbout', {
+	slidesPerView: 1.2,
+	direction: 'horizontal',
+	loop: true,
+
+	mousewheel: true,
+	keyboard: true,
+	centeredSlides: true,
+	centeredSlidesBounds: true,
+	grabCursor: true,
+
+	pagination: {
+		el: '.swiperAbout-pagination'
+	}
+})
